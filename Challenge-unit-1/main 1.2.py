6903d8bf9e96110124680bd3f5a2421ad62eda12
Ishwarya39 <147999179+Ishwarya39@users.noreply.github.com>
2023-10-16 21:50:45 +0530

    Add files via upload

---
+++ Challenge-unit-1/main 1.2.py
@@ -0,0 +1,12 @@
+def isLeapYear(year):
+  if ():
+    return True
+  else:
+    return False
+
+
+year = 2014
+if isLeapYear(year):
+  print("{}is a leap year.".format(year))
+else:
+  print("{}is not a leap year.".format(year))
